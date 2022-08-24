206be9e08ab90f5bbbdf5e809489b413ac5da408
Begzod <ebegzod477@gmail.com>
2022-08-24 13:27:12 +0500

    code03

--- bin/map_methods03.dart
+++ bin/map_methods03.dart
@@ -2,9 +2,13 @@
 Delete the "color" key from the car data map
 */
 
-void main() {}
+void main() {
+  print(func({'name': "Mustang", 'color': "silver", "prise": 50000}));
+}
 
 Map func(Map data) {
+  data.remove('color');
+
   //youre code here
   return data;
 }
