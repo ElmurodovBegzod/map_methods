86ce519a345eda764329e08d125cf0fcccfe4291
Begzod <ebegzod477@gmail.com>
2022-08-24 13:29:52 +0500

    code04

--- bin/map_methods04.dart
+++ bin/map_methods04.dart
@@ -3,9 +3,12 @@ There is a folder with login and password. If the password does not contain "@",
 add "@" to it.
 */
 
-void main() {}
+void main() {
+  print(func({'login': "John", 'password': "iameatingapple"}));
+}
 
 Map func(Map data) {
+  data['password'] = "iameatingapple@";
   //your code here
 
   return data;
