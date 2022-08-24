14560e995f76745d32c9efa74542e2edfcb6fc24
Begzod <ebegzod477@gmail.com>
2022-08-24 10:50:02 +0500

    code02

--- bin/map_methods02.dart
+++ bin/map_methods02.dart
@@ -2,9 +2,12 @@
 Car details will be given to you in map view, change the car price for it
 */
 
-void main() {}
+void main() {
+  print(func({'name': "Mustang", 'color': "silwer"}, ["price", 80000]));
+}
 
-Map func(Map data, int cost) {
+Map func(Map data, cost) {
+  data['price'] = cost;
   //your code here
 
   return data;
